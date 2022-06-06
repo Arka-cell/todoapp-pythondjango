b50faff1dfb70cf5ff51ea6003b791edeb150e8c
Samir Ahmane <55358999+Arka-cell@users.noreply.github.com>
2022-06-06 16:54:05 +0100

    feat: created TaskTypesViewset

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import status, generics, viewsets
+from todo.models import Task, TaskType
+from django.db.models import Q
 
 # Create your views here.
+
+class TaskTypesViewset(viewsets.ModelViewSet):
+    def get_queryset(self):
+        return TaskType.objects.filter(Q(user=self.request.user) | Q(user=None))
