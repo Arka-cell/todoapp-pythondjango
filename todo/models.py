b87f9590d2a57a25298568a5f0b041797ef435e5
Samir Ahmane <55358999+Arka-cell@users.noreply.github.com>
2022-06-06 16:41:50 +0100

    feat: created TaskType and Task models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class TaskType(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name="usertasktype", null=True, blank=True)
+    name = models.CharField(max_length=50)
+    
+class Task(models.Model):
+    user = models.ForeignKey(User, related_name="usertask", on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    has_finished = models.BooleanField(default=False)
