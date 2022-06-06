1785a6d6eb7e15468eca24adedb679d7703286aa
Samir Ahmane <55358999+Arka-cell@users.noreply.github.com>
2022-06-06 17:14:35 +0100

    feat: created a Signup view

--- todo/views.py
+++ todo/views.py
@@ -4,8 +4,38 @@ from rest_framework.response import Response
 from rest_framework import status, generics, viewsets
 from todo.models import Task, TaskType
 from django.db.models import Q
+from .serializers import SignUpSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
+from rest_framework.generics import CreateAPIView
+
 
 # Create your views here.
+
+class SignUpView(CreateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = SignUpSerializer
+
+    def create(self, request, *args, **kwargs):
+        """
+        POST HTTP request containing email and password
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            {**serializer.data},
+            status=status.HTTP_201_CREATED,
+            headers=headers,
+        )
+
+    def perform_create(self, serializer):
+        """
+        Perform user creation
+        """
+        user = serializer.save()
+        return user
+
 
 class TaskTypesViewset(viewsets.ModelViewSet):
     def get_queryset(self):
