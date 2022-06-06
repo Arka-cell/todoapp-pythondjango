8abbf94c2dc7740b972dac02f2f2d2c53085b810
Samir Ahmane <55358999+Arka-cell@users.noreply.github.com>
2022-06-06 17:14:49 +0100

    feat: added sign-up route and login route

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TaskTypesViewset
+from .views import TaskTypesViewset, SignUpView
+from rest_framework.authtoken.views import ObtainAuthToken
 
 urlpatterns = [
     path("task-types/", TaskTypesViewset.as_view({"get": "list", "post": "create"}), name="tasktypes"),
     path("task-types/<int:pk>", TaskTypesViewset.as_view({"get": "retrieve", "patch": "update"}), name="tasktype"),
+    path("login/", ObtainAuthToken.as_view(), name="login"),
+    path("sign-up/", SignUpView.as_view(), name="signup")
 ]
