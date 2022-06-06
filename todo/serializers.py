3d40b025945273e4fc15ab4d7994a5a667e1906f
Samir Ahmane <55358999+Arka-cell@users.noreply.github.com>
2022-06-06 17:14:22 +0100

    feat: created a SignUpSerializer for user

---
+++ todo/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from todo.models import User
+from django.contrib.auth.forms import PasswordResetForm
+
+class SignUpSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, required=True, style={"input_type": "password"}
+    )
+
+    class Meta:
+        model = User
+        fields = ["email", "password", "username"]
+        extra_kwargs = {"email": {"required": True}, "username": {"required": True}}
+
+    def validate_email(self, value):
+        self.form = PasswordResetForm(data=self.initial_data)
+
+        if not self.form.is_valid():
+            raise serializers.ValidationError(self.form.errors)
+        return value
+
+    def send_mail(self, **kwargs):
+        pass
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
