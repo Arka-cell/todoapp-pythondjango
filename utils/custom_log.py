cda8e1b83d70b192a91a57ba3067ed87b5207c83
Samir Ahmane <55358999+Arka-cell@users.noreply.github.com>
2022-06-08 21:51:29 +0100

    feat: added custom_log

---
+++ utils/custom_log.py
@@ -0,0 +1,45 @@
+import logging
+
+
+class CustomFormatter(logging.Formatter):
+    """
+    Custom formatter for logging module to colorize logger message
+    """
+
+    grey = "\x1b[32;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = (
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    )
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset,
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+log = logging.getLogger(__name__)
+
+log.setLevel(logging.INFO)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+ch.setFormatter(CustomFormatter())
+
+log.addHandler(ch)
+
+
+def get_log():
+    return log
