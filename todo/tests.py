7e264c7872992c66d15c1f8fec8d75e6aecf02f0
Samir Ahmane <55358999+Arka-cell@users.noreply.github.com>
2022-06-08 21:51:54 +0100

    feat: added setUp for AccountTest

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.reverse import reverse
+from todo.models import User
+from rest_framework.test import force_authenticate
+from rest_framework.test import APIRequestFactory
+from rest_framework.authtoken.views import obtain_auth_token
+from utils.custom_log import get_log
+
+log = get_log()
 
 # Create your tests here.
+
+class AccountTest(APITestCase):
+    def setUp(self) -> None:
+        self.data = {
+            "email": "test@email.com",
+            "username": "testuser",
+            "password": "s0m$Password",
+        }
+        self.user = User.objects.create(username=self.data["username"])
+        self.user.set_password(self.data["password"])
+        self.user.save()
+        self.token = None 
+        self.sign_up_data = {
+            "email": "signup@email.com",
+            "username": "signupuser",
+            "password": "s0m$Password",
+        }
+    
